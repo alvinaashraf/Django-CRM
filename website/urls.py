4fce3ab24af6ca968c7783d0a3c66b04e1df6cbf
alvinaashraf <alvina1409b@aptechgdn.net>
2023-06-11 17:18:20 +0500

    'final

--- website/urls.py
+++ website/urls.py
@@ -6,6 +6,13 @@ from . import views
 urlpatterns = [
     path('', views.home,name="home"),
     path('logout/',views.logout_user,name="logout"),
-    path('register/',views.register_user,name="register")
+    path('register/',views.register_user,name="register"),
+    path('add_record/',views.add_record,name="add_record"),
+
+    path('record_detail/<int:pk>',views.record_detail,name="record_detail"),
+        path('update_record/<int:pk>',views.update_record,name="update_record"),
+
+    path('record_delete/<int:pk>',views.record_delete,name='record_delete')
+
    
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import RegisterForm ,add_record_form
+from .models import Record
 # Create your views here.
 def home(request):
+    
+    # display records in table 
+    record=Record.objects.all()
+    
+    #display records in table end 
     # check to see if user is logged in 
     if request.method == 'POST':
         username = request.POST['username']
@@ -18,7 +25,7 @@ def home(request):
             return redirect('home')
         
     else:
-        return render(request,'home.html',{})
+        return render(request,'home.html',{'record':record})
 
 
 
@@ -32,6 +39,79 @@ def logout_user(request):
 
 
 def register_user(request):
-            return render(request,'register.html',{})
+    if request.method=='POST':
+        form= RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            # authenticate and login 
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,' Remember your username and password registered ')
+            return redirect('home')
+        
+    else:
+        form=RegisterForm()
+        return render(request,'register.html',{'form':form})
+    return render(request,'register.html',{'form':form})
+
+
+    
+def record_detail(request,pk):
+    if request.user.is_authenticated:
+        cust_rec=Record.objects.get(id=pk)
+        return render(request,'record_detail.html',{'cust_rec':cust_rec})
+    
+    else:
+        messages.success(request,' user must be logged in to view ')
+        return redirect('home')
+
+
+def record_delete(request,pk):    
+    if request.user.is_authenticated:
+        delete_obj=Record.objects.get(id=pk)
+        delete_obj.delete()
+        messages.success(request,' YOU HAVE DELETED A USER SUCCESSFULLY ')
+        return redirect('home')
+    else:
+        messages.success(request,' error in deleting must have done something wrong ')
+        return redirect('home')
+
+    
+    
+    
+def add_record(request):
+    form=add_record_form(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_rec=form.save()
+                messages.success(request,'Record Added Successfully')
+                return redirect('home')
+        return render(request,'add_record.html',{'form':form})
+    else:
+        messages.success(request,'Your Must be Logged In to Add a Record')
+        return redirect('home')
+        
+    
+    
+    
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        current_rec=Record.objects.get(id=pk)
+        form=add_record_form(request.POST or None,instance=current_rec)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Record Updated Successfully')
+            return redirect('home')
+        
+        return render(request,'update_record.html',{'form':form})
+    
+    else:
+        messages.success(request,'Your Must be Logged In to update a Record')
+        return redirect('home')
+        
 
     
